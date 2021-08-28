49d6de84638cfd42d2eb8b5892d4e1da71eac4a4
ignatius-one <josegnacio118@gmail.com>
2021-08-27 22:43:04 -0500

    add efect writing header

--- js/scrip.js
+++ js/scrip.js
@@ -1,9 +1,13 @@
+// Aos 
+    AOS.init();
 
+// change icon 
 document.getElementById('changes-icon').addEventListener('click', () => {
     document.getElementById('menu').classList.toggle('show-menu')
     document.getElementById('changes-icon').classList.toggle('bar-x')
 })
 
+// hidden nav 
 let position = window.pageYOffset;
 let nav = document.getElementById('nav');
 window.addEventListener('scroll', function () {
@@ -17,8 +21,24 @@ window.addEventListener('scroll', function () {
 })
 
 
-
+// my dark mode 
 document.getElementById('dark-mode').addEventListener('click', () => {
     document.body.classList.toggle('dark-active')
     document.getElementById('dark-mode').classList.toggle('change')
+})
+
+// type.js
+const typed = new Typed('.typed', {
+    strings: ['cat'],
+    typeSpeed: 80,
+    startDelay: 300,
+	backSpeed: 75, 
+	shuffle: false,
+	backDelay: 1500,
+	loop: true, 
+	loopCount: false,
+	showCursor: true,
+	cursorChar: '|', 
+	contentType: 'html', 
+
 })
