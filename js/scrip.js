9987deb586bd79b0885120ebd9a38eb071d94cb3
ignatius-one <josegnacio118@gmail.com>
2021-09-07 14:19:46 -0500

    add icon

--- js/scrip.js
+++ js/scrip.js
@@ -31,51 +31,14 @@ document.getElementById('dark-mode').addEventListener('click', () => {
     document.getElementById('dark-mode').classList.toggle('change')
 })
 
-// type.js
-// const typed = new Typed('.typed', {
-//     strings: ['Developer Web',' Designer Web','or..','waiit!','aa yeaa','Front end','yes'],
-//     typeSpeed: 80,
-//     startDelay: 300,
-// 	backSpeed: 75, 
-// 	shuffle: false,
-// 	backDelay: 1500,
-// 	loop: true, 
-// 	loopCount: false,
-// 	showCursor: true,
-// 	cursorChar: '|', 
-//     contentType: 'html',
-// })
-
-
-
-// progress bars 
-// setInterval(() => {
-//    document.getElementsByTagName('progress')[0].value =
-//    document.getElementsByTagName('progress')[0].value + 1
-//    if (document.getElementsByTagName('progress')[0].value === 80) {
-//        document.getElementsByTagName('progress')[0].value = 1
-//     }
-// }, 250);
-
 
 function openPage(pageName) {
   // Hide all elements with class="tabcontent" by default */
-  let i, tabcontent, tablinks;
+  let i, tabcontent;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-
-  // Remove the background color of all tablinks/buttons
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
-
-  // Show the specific tab content
   document.getElementById(pageName).style.display = "block";
-  // Add the specific color to the button used to open the tab content
 }
-
-// Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
